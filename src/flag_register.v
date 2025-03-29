c2cde8bb1dc4438739982a277931c2d448ba3f0d
vedatenisgul <gulv22@itu.edu.tr>
2025-03-29 18:28:35 +0300

    flag register eklendi

---
+++ src/flag_register.v
@@ -0,0 +1,18 @@
+module flag_register(clock, Z, C, N, O, flag_reg, carry);
+    input wire clock;
+    input wire Z;  
+    input wire C;  
+    input wire N;  
+    input wire O;  
+
+    output reg [3:0] flag_reg;  
+
+    output wire carry; 
+
+    always @(posedge clock) begin
+        flag_reg <= {Z, C, N, O}; 
+    end
+
+    assign carry = flag_reg[2];  
+
+endmodule
