8bb9c2f8f5e75c0b0f1aa8833718ac098d543d2e
vedatenisgul <gulv22@itu.edu.tr>
2025-03-31 21:42:52 +0300

    muxc updated

--- src/mux_2_4_2.v
+++ src/mux_2_4_2.v
@@ -5,16 +5,16 @@ module mux_2_4_2 (clock, select, i0, i1, i2, i3, o);
     input wire [7:0] i1;
     input wire [7:0] i2;
     input wire [7:0] i3;
-    output reg [31:0] o;
+    output reg [7:0] o;
 
     always @(posedge clock)
     begin
         case (select)
-            2'b00: o <= {{24{i0[7]}}, i0};
-            2'b01: o <= {{24{i1[7]}}, i1};
-            2'b10: o <= {{24{i2[7]}}, i2};
-            2'b11: o <= {{24{i3[7]}}, i3};
-            default: o <= 32'b0;
+            2'b00: o <= i0;
+            2'b01: o <= i1;
+            2'b10: o <= i2;
+            2'b11: o <= i3;
+            default: o <= 8'b0;
         endcase
     end
 
