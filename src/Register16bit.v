ff1f7eecad53300e3ff8c90dd7229ae5eeb96f09
vedatenisgul <gulv22@itu.edu.tr>
2025-04-01 15:06:13 +0300

    updated

--- src/Register16bit.v
+++ src/Register16bit.v
@@ -1,14 +1,14 @@
 module Register16bit(I, E, FunSel, Clock, Q);
 	
 	//inputs
-	input wire clock; // clocku kafamıza göre mi belirliyoruz  posedge ya da negedge olduğunu bilmiyorum???
+	input wire Clock; // clocku kafamıza göre mi belirliyoruz  posedge ya da negedge olduğunu bilmiyorum???
 	input wire E;
 	input wire [1:0] FunSel;
 	input wire [15:0] I;
 	//outputs, alwaysin içinde sadece reg güncellenebiliyormuş
 	reg [15:0] Q;
 	
-	always @(posedge clock)
+	always @(posedge Clock)
 	begin
 
         if(E)
