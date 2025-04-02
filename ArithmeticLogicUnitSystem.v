2db7f96d4dad34b37c69cc5e9a824ef69ceedc4e
Vedat Enis Gül <145773536+vedatenisgul@users.noreply.github.com>
2025-04-02 19:14:52 +0300

    Update ArithmeticLogicUnitSystem.v

--- ArithmeticLogicUnitSystem.v
+++ ArithmeticLogicUnitSystem.v
@@ -1,13 +1,12 @@
 `timescale 1ns / 1ps
 
-module mux_1_2 (clock, select, i0, i1, o);
-    input wire clock;
+module mux_1_2 (select, i0, i1, o);
     input wire select;
     input wire [31:0] i0;
     input wire [15:0] i1;
     output reg [31:0] o;
 
-    always @(posedge clock)
+    always @(*)
     begin
         if(select)
             o <= {{16{i1[15]}}, i1};
@@ -16,8 +15,7 @@ module mux_1_2 (clock, select, i0, i1, o);
     end
 endmodule
 
-module mux_2_4 (clock, select, i0, i1, i2, i3, o);
-    input wire clock;
+module mux_2_4 (select, i0, i1, i2, i3, o);
     input wire [1:0] select;
     input wire [31:0] i0;
     input wire [15:0] i1;
@@ -25,22 +23,21 @@ module mux_2_4 (clock, select, i0, i1, i2, i3, o);
     input wire [7:0] i3;
     output reg [31:0] o;
 
-    always @(posedge clock)
+    always @(*)
     begin
         case (select)
             2'b00: o <= i0;
             2'b01: o <= {{16{i1[15]}}, i1};
             2'b10: o <= i2;
             2'b11: o <= {{24{i3[7]}}, i3};
-            default: o <= 32'b0;
+            default: o <= o;
         endcase
     end
 
 
 endmodule
 
-module mux_2_4_2 (clock, select, i0, i1, i2, i3, o);
-    input wire clock;
+module mux_2_4_2 (select, i0, i1, i2, i3, o);
     input wire [1:0] select;
     input wire [7:0] i0;
     input wire [7:0] i1;
@@ -48,14 +45,14 @@ module mux_2_4_2 (clock, select, i0, i1, i2, i3, o);
     input wire [7:0] i3;
     output reg [7:0] o;
 
-    always @(posedge clock)
+    always @(*)
     begin
         case (select)
             2'b00: o <= i0;
             2'b01: o <= i1;
             2'b10: o <= i2;
             2'b11: o <= i3;
-            default: o <= 8'b0;
+            default: o <= o;
         endcase
     end
 
@@ -143,7 +140,6 @@ module ArithmeticLogicUnitSystem(RF_OutASel, RF_OutBSel, RF_FunSel, RF_RegSel, R
     );
 
     mux_1_2 mux_D (
-        .clock(Clock),
         .select(MuxDSel),
         .i0(RFOutA),
         .i1(ARFOutC),
@@ -154,7 +150,7 @@ module ArithmeticLogicUnitSystem(RF_OutASel, RF_OutBSel, RF_FunSel, RF_RegSel, R
         .A(MuxDOut),
         .B(RFOutB),
         .FunSel(ALU_FunSel),
-        .WF(FlagCarryOut),
+        .WF(ALU_WF),
         .Clock(Clock),
         .ALUOut(ALUOut),
         .FlagsOut(FlagsOut)
@@ -167,7 +163,6 @@ module ArithmeticLogicUnitSystem(RF_OutASel, RF_OutBSel, RF_FunSel, RF_RegSel, R
     );
 
     mux_2_4_2 mux_C (
-        .clock(Clock),
         .select(MuxCSel),
         .i0(ALUOut[7:0]),
         .i1(ALUOut[15:8]),
@@ -212,7 +207,6 @@ module ArithmeticLogicUnitSystem(RF_OutASel, RF_OutBSel, RF_FunSel, RF_RegSel, R
         .OutD(ARFOutD)
     );
     mux_2_4 mux_A (
-        .clock(Clock),
         .select(MuxASel),
         .i0(ALUOut),
         .i1(ARFOutC),
@@ -222,7 +216,6 @@ module ArithmeticLogicUnitSystem(RF_OutASel, RF_OutBSel, RF_FunSel, RF_RegSel, R
     );
 
     mux_2_4 mux_B (
-        .clock(Clock),
         .select(MuxBSel),
         .i0(ALUOut),
         .i1(ARFOutC),
